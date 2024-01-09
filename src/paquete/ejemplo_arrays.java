3b457bca8ed721ac05eea43a618a54e017466a57
Mañana <Maana@DESKTOP-RCDFL5S>
2024-01-09 09:44:39 +0100

    Start con ejemplo de como funcionan los vectores o arrays.

---
+++ src/paquete/ejemplo_arrays.java
@@ -0,0 +1,32 @@
+package paquete;
+
+public class ejemplo_arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char frase[] = new char[10]; 
+
+		 
+
+		frase[0] = 'H'; 
+		frase[1] = 'o'; 
+		frase[2] = 'l'; 
+		frase[3] = 'a'; 
+		frase[4] = ' '; 
+		frase[5] = 'h'; 
+		frase[6] = 'o'; 
+		frase[7] = 'l'; 
+		frase[8] = 'a'; 
+		frase[9] = '!'; 
+
+		//Los dos puntos ":" significa que por letra pase por todo lo que hay en frase "0,1,2...".
+		
+		for(char letra: frase)	{
+			System.out.println("Bucle avanzado " +letra);
+		}
+			
+				
+	}
+
+}
